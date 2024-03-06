1dc1debefc316cfded9033e3ef3a34ea51f03442
Sven Kröger <svenni.kroeger@gmail.com>
2024-03-06 17:16:18 +0100

    fix: updating refs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,46 @@ export default defineNuxtConfig({
         iso: 'en-US',
         file: 'fetchTranslations.js',
       },
+      {
+        code: 'fr',
+        iso: 'en-US',
+        file: 'fetchTranslations.js',
+      },
+      {
+        code: 'ar',
+        iso: 'en-US',
+        file: 'fetchTranslations.js',
+      },
+      {
+        code: 'hi',
+        iso: 'en-US',
+        file: 'fetchTranslations.js',
+      },
+      {
+        code: 'hr',
+        iso: 'en-US',
+        file: 'fetchTranslations.js',
+      },
+      {
+        code: 'nl',
+        iso: 'en-US',
+        file: 'fetchTranslations.js',
+      },
+      {
+        code: 'sk',
+        iso: 'en-US',
+        file: 'fetchTranslations.js',
+      },
+      {
+        code: 'sl',
+        iso: 'en-US',
+        file: 'fetchTranslations.js',
+      },
+      {
+        code: 'vi',
+        iso: 'en-US',
+        file: 'fetchTranslations.js',
+      },
     ],
     experimental: {
       jsTsFormatResource: true,
